cb235e4e437f6b6c59044897b15ac7bc754a34c4
David Hajnal <hajnaldave@gmail.com>
2018-05-31 20:52:28 +0200

    result.js fixes

--- result.js
+++ result.js
@@ -1,20 +1,17 @@
-//result script
+//Result script
 
-
-//Todo: Ittmegvizygálni, hogy elágazásokban szükség van e paraméter átadásokra!
-//Todo:Pokerkezek sorba rendezése!
 if(isRoyalFlush()){
     document.querySelector('#hand').innerHTML += '<br>Royal Flush!';
 }else if(isStraightFlush()){
     document.querySelector('#hand').innerHTML += '<br>Straight Flush!';
-}else if(isFlush()){
-    document.querySelector('#hand').innerHTML += '<br>Flush!';
-}else if(isStraight()) {
-    document.querySelector('#hand').innerHTML += '<br>Straight!';
 }else if(isPoker()){
     document.querySelector('#hand').innerHTML += '<br>Poker!';
 }else if(isFull()){
     document.querySelector('#hand').innerHTML += '<br>Full House!';
+}else if(isFlush()){
+    document.querySelector('#hand').innerHTML += '<br>Flush!';
+}else if(isStraight()) {
+    document.querySelector('#hand').innerHTML += '<br>Straight!';
 }else if(isDrill()){
     document.querySelector('#hand').innerHTML += '<br>Drill!';
 }else if(twoPairs()){
